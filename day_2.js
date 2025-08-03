1b704e96113103e5dc25a6b82193576cbc69aaab
MokshJain0904 <mokshj002@gmail.com>
2025-08-03 11:15:35 +0530

    day_2.js

--- day_2.js
+++ day_2.js
@@ -36,7 +36,39 @@ console.log(`Zip code is: ${myObject.address.zipcode}`);*/
 //     }
 //     console.log(sum);
 
-//
+// a is accumulator its a symbol
+
+/*let arr=[];
+for(i=1;i<=100;i++)arr.push(i);
+let b=arr.map(x=>x*3).filter(x=>x%5===0).reduce((a,x)=>a+x);
+console.log(b);*/
+// or u could just console log the whole statement
+/*map if only for modification of values whiles,
+filter is used to return as conditon eg:-x=>x%5==0;
+a is accumulator*/1305
+
+//sum of positive numbers
+/*let arr=[3,-4,5,-6,67];
+let b= arr.filter(x=>x>0).reduce((a,x)=>a+x);
+console.log(b);*/
+
+
+/*users of i= users.[i]se me array access karta hu
+and then .age se mein age access karta hu*/
+
+
+/*const users=[
+    { name:'Alice',age:25},
+    { name:'Bob',age:30},
+    { name:'Charlie',age:25},
+];
+let sum=0;
+for(let i=0;i<3;i++){
+sum= sum+ users[i].age;}
+let avg=sum/3;
+console.log(avg);*/
+
+
 
 
 
